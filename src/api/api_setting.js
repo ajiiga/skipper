74fe9eb4fdbfe1b5bdf48115676aa5267cc0fc6d
Vasil <barsegyan.vasil@gmail.com>
2022-04-11 17:45:02 +0500

    change api url

--- src/api/api_setting.js
+++ src/api/api_setting.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import authStore from "../store/authStore";
 
-export let API_URL = 'http://152.70.189.77:8000'
+export let API_URL = 'http://152.70.189.77/backend'
 
 let $api = axios.create({
     baseURL: API_URL

0e6e7b92a286494dfbcb9af45b2d3bb3e466166c
Vasil <barsegyan.vasil@gmail.com>
2022-05-24 13:59:18 +0500

    fix header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -14,6 +14,7 @@ import authStore from "../../store/authStore";
 import MyLessonsButton from "../UI/MyLessonsButton/MyLessonsButton";
 import MobileMenu from "./MobileMenu/MobileMenu";
 import messagesStore from "../../store/messagesStore";
+import NotificationModal from "./NotificationModal/NotificationModal";
 
 const Header = ({isAuth, profile}) => {
 
